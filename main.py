27e6ee4e0b7fe9c66f776ab9ddad234629ecd3f0
jayshain <jayshain0904@gmail.com>
2021-11-25 15:21:11 +0800

    新增pm25 1.更新排序功能 2.新增css跟最高

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 from datetime import datetime
 import pandas as pd
 
 app=Flask(__name__)#本地端
 
 #建立router
-@app.route('/pm25')
+@app.route('/pm25' , methods=['GET','POST'])
 def pm25():
     url='https://data.epa.gov.tw/api/v1/aqx_p_02?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&sort=ImportDate%20desc&format=csv'
     df = pd.read_csv(url)[['county','Site','PM25','ItemUnit']]
+    df.dropna(inplace=True)
     columns=df.columns
     values=df.values.tolist()
     time = today()
+    pm25_sort = sorted(values,key=lambda x:x[2],reverse=True)
+    result_sort = pm25_sort[0][1],pm25_sort[0][2]
+
+    if request.method =='POST':
+        if request.form.get('sort'):
+            values = pm25_sort
+
 
     return render_template('pm25.html', **locals())
 
