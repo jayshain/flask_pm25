d390319c414af21a6dcaf5fef5c2febe88b5c2c3
jayshain <jayshain0904@gmail.com>
2021-11-25 12:09:28 +0800

    測試跟新增

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from flask import Flask,render_template
 from datetime import datetime
+import pandas as pd
+
 app=Flask(__name__)#本地端
 
 #建立router
+@app.route('/pm25')
+def pm25():
+    url='https://data.epa.gov.tw/api/v1/aqx_p_02?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&sort=ImportDate%20desc&format=csv'
+    df = pd.read_csv(url)[['county','Site','PM25','ItemUnit']]
+    columns=df.columns
+    values=df.values.tolist()
+    time = today()
+
+    return render_template('pm25.html', **locals())
+
+
+
 @app.route('/')
 def index():
     name = 'Calvin'
